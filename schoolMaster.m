be92aba1ac7327dc802ab1a00f781b428ec414ed
wujianqiang <505450379@qq.com>
2015-09-01 13:56:54 +0800

    add master
    master

---
+++ schoolMaster.m
@@ -0,0 +1,13 @@
+//
+//  schoolMaster.m
+//  NetWork
+//
+//  Created by wujianqiang on 15/9/1.
+//  Copyright (c) 2015年 wujianqiang. All rights reserved.
+//
+
+#import "schoolMaster.h"
+
+@implementation schoolMaster
+
+@end
